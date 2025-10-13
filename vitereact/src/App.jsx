f72cb4dd971b78e5875fb5512f0c0be1dd90f6d9
Naman <namansingla@gmail.com>
2025-10-13 21:22:38 +0530

    complete project

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-center text-3xl font-bold mb-8">Hello Naman Welcome To Tailwind Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
